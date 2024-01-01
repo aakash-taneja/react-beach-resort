e39f9aee7590b74bd8b99aedcdd37d2160c725f2
Aakash Taneja <56475445+aakash-taneja@users.noreply.github.com>
2024-01-01 12:24:59 +0530

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,27 +10,27 @@ class Services extends Component {
     
         this.state = {
              services:[
-                 {
-                     icon:<FaCocktail/>,
-                     title:"free cocktails",
-                     info: "sdf sdf sdf sdfs fs dfsd fsf werfw erfw ef ecf"                
-                 },
-                 {
-                    icon:<FaHiking/>,
-                    title:"free cocktails",
-                    info: "sdf sdf sdf sdfs fs dfsd fsf werfw erfw ef ecf"                
-                },
-                {
-                    icon:<FaShuttleVan/>,
-                    title:"free cocktails",
-                    info: "sdf sdf sdf sdfs fs dfsd fsf werfw erfw ef ecf"                
-                },
-                {
-                    icon:<FaBeer/>,
-                    title:"free cocktails",
-                    info: "sdf sdf sdf sdfs fs dfsd fsf werfw erfw ef ecf"                
-                }
-             ]
+              {
+                icon: <FaCocktail />,
+                title: "Free Cocktails",
+                info: "Enjoy complimentary cocktails during your stay. Our skilled bartenders are ready to serve you the finest drinks."
+              },
+              {
+                icon: <FaHiking />,
+                title: "Hiking Adventures",
+                info: "Embark on exciting hiking adventures with breathtaking views. Our guided tours cater to all skill levels, from beginners to experienced hikers."
+              },
+              {
+                icon: <FaShuttleVan />,
+                title: "Convenient Shuttle Service",
+                info: "Take advantage of our complimentary shuttle service. We provide convenient transportation to nearby attractions, making your travel hassle-free."
+              },
+              {
+                icon: <FaBeer />,
+                title: "Craft Beer Selection",
+                info: "Indulge in a diverse selection of craft beers. Our extensive beer menu ensures that you can savor unique and flavorful options during your stay."
+              }
+            ]
         }
     }
     
